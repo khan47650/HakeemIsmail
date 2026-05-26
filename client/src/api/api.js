96bdf204c99ff8fbdb3363a45e759dd0a7907346
khan47650 <bsem-f20-110@superior.edu.pk>
2026-05-26 14:40:50 +0500

    url error fixed

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api.hakeem-ismail.com",
+    baseURL: "https://api.hakeem-ismail.com/api",
 });
 
 export default api;
